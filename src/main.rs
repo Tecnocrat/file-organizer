416f2b70c6d75f4f54dcc13d72325a33b7549c00
tecnocrat <jesussard@gmail.com>
2025-05-12 00:46:14 +0200

    Implement CLI argument parsing for file organization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about ="CLI File Organizer in Rust")]
+struct Cli {
+    /// The path to the directory to organize
+    #[arg(short, long)]
+    path: String,
+
+    /// Organizing mode: [by_extension, by_date]
+    #[arg(short, long)]
+    mode: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Organizing {} using mode: {}", args.path, args.mode);
+
 }
